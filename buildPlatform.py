28364163c265002d5191bd3bcb4b2c0ed3e443c4
Tobias Blaufuß <t.blaufuss@gmx.de>
2018-05-25 19:25:35 +0200

    new build script (windows and linux)

--- buildPlatform.py
+++ buildPlatform.py
@@ -2,15 +2,27 @@
 
 from subprocess import check_call
 import os
+import platform
+import sys
 
 with open('languages.txt') as fp:
+	current_system = platform.system()
+	if current_system == "Windows":
+		shell=True
+	elif current_system == "Linux":
+		shell=False
+	else:
+		print('Unknown system platform: {}'.format(current_system))
+		sys.exit(1)
+	print('Executing build on system {}. Execute in additional shell={}'.format(current_system, shell))
+	
 	line = fp.readline()
 	cnt = 1
 	while line:
 		line_formated = line.strip().replace('\n', '')
-		print('Visiting {}'.format(line_formated))
-		path = os.path.join(line.replace('\n', ''))
+		path = os.path.join(line_formated)
+		print('Visiting {}'.format(path))
 		print('Executing "gradle build" in "{}"'.format(path))
-		check_call(['gradle', 'build'], cwd=path)
+		check_call(['gradle', 'build'], cwd=path, shell=shell)
 		line = fp.readline()
 		cnt += 1
