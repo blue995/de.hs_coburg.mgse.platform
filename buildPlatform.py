e06fc1afa3992c503405bec36d9af1507479642d
Tobias Blaufuß <t.blaufuss@gmx.de>
2018-05-25 17:58:52 +0200

    new build script

--- buildPlatform.py
+++ buildPlatform.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from subprocess import check_call
-
+import os
 
 with open('languages.txt') as fp:
 	line = fp.readline()
@@ -9,7 +9,8 @@ with open('languages.txt') as fp:
 	while line:
 		line_formated = line.strip().replace('\n', '')
 		print('Visiting {}'.format(line_formated))
-		check_call(['gradle', 'build'], 
-cwd='./'+line.replace('\n', ''))
+		path = os.path.join(line.replace('\n', ''))
+		print('Executing "gradle build" in "{}"'.format(path))
+		check_call(['gradle', 'build'], cwd=path)
 		line = fp.readline()
 		cnt += 1
